856f07216ea2713f0d59faa697d676e2a1d0a3f3
Andrew Guernsey <>
2021-05-03 20:19:06 +0000

    10%

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ import time
 import random
 from os import system, name
 
+### VARS ###
+badjokes = [
+ "Why is it a bad idea to iron your four-leaf clover? Cause you shouldn't press your luck.",
+ "I ordered a chicken and an egg from Amazon. I'll let you know.",
+ "I can't take my dog to the pond anymore because the ducks keep attacking him. That's what I get for buying a pure bread dog.",
+ "My wife said I was immature. So I told her to get out of my fort.",
+ "I didn't want to believe that my dad was stealing from his job as a traffic cop, but when I got home, all the signs were there.",
+ "I spent a lot of time, money, and effort childproofing my house… but the kids still get in.",
+ "What rock group has four men that don't sing? Mount Rushmore.",
+ "How many tickles does it take to make an octopus laugh? It takes ten... ten tickles!",
+ "What do sprinters eat before a race? Nothing, they fast!",
+ "What happens when you go to the bathroom in France? European.",
+ "Why did the invisible man turn down the job offer? He couldn't see himself doing it!",
+ "Want to hear a joke about construction? I'm still working on it!",
+ 'I was really angry at my friend Mark for stealing my dictionary. I told him, "Mark, my words!"',
+ "What is the tallest building in the world? The library—it's got the most stories.",
+ "Within minutes, the detectives knew what the murder weapon was. It was a brief case.",
+ "My wife told me I had to stop acting like a flamingo. So I had to put my foot down!",
+ "I don't play soccer because I enjoy the sport. I'm just doing it for kicks!"
+ ]
+ 
+
 ### FUNCTIONS ###
 
 ## Clear Function from geeksforgeeks ##
@@ -63,6 +85,8 @@ clear()
 
 ## Login ##
 login()
+
+## Main Menu ##
 while True:
     clear()
     try:
@@ -75,6 +99,8 @@ Welcome to Snek OS! Your options are listed below!
 4 - Diagnose Computer
 5 - Not a virus.exe
 6 - Die
+
+9 - Logout
 
 """))
     except ValueError:
@@ -122,15 +148,41 @@ Time in game: 0.01
 If you feel like this is a false ban, please don't bother contacting support. We don't care!
 """)
                 continue
-            elif steamput == 9:
+            elif steamput == 9: #Steam/Back
                 break
             else:
                 continue
         continue
-    elif sysput == 3:
+    elif sysput == 3: #Dad Jokes
         clear()
+        input(random.choice(badjokes))
         continue
-    elif sysput == 4:
+    elif sysput == 4: #Diagnose
+        while True:
+            clear()
+            try:
+                diagput = int(input("""
+Welcome to the computer diagnosis tool! Would you like to view your computer's status?
+1 - Yes
+2 - No
+
+9 - Back
+"""))
+            except ValueError:
+                continue
+            if diagput == 1: #Diagnose/yes
+                clear()
+                input("According to my sources, your computer is ON. You are welcome!")
+                break
+            elif diagput == 2: #Diagnose/no
+                clear()
+                input("Why did you come then?")
+                break
+            elif diagput == 9: #Diagnose/back
+                clear()
+                break
+            else:
+                continue
         clear()
         continue
     elif sysput == 5:
@@ -139,4 +191,8 @@ If you feel like this is a false ban, please don't bother contacting support. We
     elif sysput == 6:
         clear()
         input("You are now dead. Congradulations! Now what...")
+        continue
+    elif sysput == 9:
+        clear()
+        login()
         continue
